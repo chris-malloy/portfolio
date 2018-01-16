fb72549d0dbde8298fa4766ddc6d35ba438b5a16
chris-malloy <chrismalloymusic@gmail.com>
2018-01-16 17:00:43 -0500

    update current projects, refined layout

--- public/javascripts/scripts.js
+++ public/javascripts/scripts.js
@@ -1,8 +1,10 @@
 $(document).ready(() => {
+    console.log('thing')
     $('#splash').hide();
     // $('#loading').hide();
     setTimeout(() => {
         $('#loading').fadeOut();
         $('#splash').fadeIn(1000);
     }, 1000)
-})
+});
+
